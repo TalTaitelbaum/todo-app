3fd29875c1bbe94c2f4cbd22fde7b38604289818
Tal Taitelbaum <talbak.tb@gmail.com>
2022-12-05 14:31:11 +0200

    Using conditional operator on getSavedTodos

--- todos-functios.js
+++ todos-functios.js
@@ -1,12 +1,7 @@
 //Fetch existing todo from localStorage
 const getSavedTodos = () => {
   const todosJSON = localStorage.getItem("todos");
-
-  if (todosJSON !== null) {
-    return JSON.parse(todosJSON);
-  } else {
-    return [];
-  }
+  return todosJSON !== null ? JSON.parse(todosJSON) : [];
 };
 
 //Save todos in localStorage
